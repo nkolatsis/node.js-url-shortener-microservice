846438c46e53cf9cd36cd4511adf71019c28c15f
Nicholas Kolatsis <13288906+nkolatsis@users.noreply.github.com>
2018-06-20 12:01:20 +0200

    Updated the shorturl creation method.

--- shorten-ms.js
+++ shorten-ms.js
@@ -5,20 +5,20 @@ mongoose.connect(process.env.MONGO_URI);
 const Schema = mongoose.Schema;
 
 const URLMapSchema = new Schema({
+  url: {
+    type: String,
+    required: true,
+  },
   short: {
       type: String,
       required: true,
       unique: true
-  },
-  url: {
-    type: String,
-    required: true,
   }
 });
 
 const URLMap = mongoose.model('URLMap', URLMapSchema);
 
-function uniqueSlug() {
+function getUniqueSlug() {
   let unique = false;
   let generatedURL = new Array(6);
 
@@ -37,30 +37,29 @@ function uniqueSlug() {
 }
 
 app.get("/api/shorturl/:short", (req, res) => {
-    console.log("/" + req.param.short + " requested.")
-    /* search db and redirect else 404 */
-    URLMap.findOne({short: req.params.short}, (err, data) => {
-      if (err) console.log(err);
-      if (!data) {
-        res.redirect(data.url);
-      } else {
-        res.status(404).send(req.params.short + " does not exist in the system.");
-      }
-    });
+  console.log("/api/shorturl/" + req.param.short + " requested.")
+  /* search db and redirect else 404 */
+  URLMap.findOne({short: req.params.short}, (err, data) => {
+    if (err) console.log(err);
+    if (!data) {
+      res.redirect(data.url);
+    } else {
+      res.status(404).send(req.params.short + " does not exist in the system.");
+    }
+  });
 })
 
 app.post("/api/shorturl/new", (req, res) => {
-    console.log("/new/" + req.param.url + "requested.")
-    /* search db for duplicates then add or not and return the json */
-    urls.find({original: req.params.url})
-        .toArray((err, url) =>  {
-            if (url[0] == undefined) {
-                var urlModel = {short_url: uniqueSlug(), original_url: req.params.url}
-                urls.insertOne(urlModel, (err, data) => {
-                    if (err) raise: err
-                })
-            }
-        })
-})
+  /* search db for duplicates then add or not and return the json */
+  let urlMap = new URLMap({url: req.params.url, short: getUniqueSlug()});
+  urlMap.save((err, data) => {
+    if (err) console.log(err);
+    res.redirect(data.short)
+  });
+});
+
+app.get("/", (req, res) => {
+
+});
 
 app.listen(5000, () => console.log("Microservice running on port 5000"))
